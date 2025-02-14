6b6969073175b6d888631243eff9de5b92bfc2ca
Daniels Šatcs <shatz.dan@gmail.com>
2025-02-14 12:23:34 +0200

    Temporary hotfix for plasma 6.3 until https://github.com/paulmcauley/klassy/pull/178 is merged.

--- flake.nix
+++ flake.nix
@@ -1,75 +1,60 @@
 {
     inputs = {
           nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+          flake-utils.url = "github:numtide/flake-utils";
     };
-    outputs = {self, nixpkgs, ...}:
-        let
-            inherit (nixpkgs.lib) genAttrs;
+    outputs = {self, nixpkgs, flake-utils, ...}:
 
-            eachSystem = f: genAttrs
-            [
-                "aarch64-darwin"
-                "aarch64-linux"
-                "x86_64-darwin"
-                "x86_64-linux"
-            ]
-            (system: f nixpkgs.legacyPackages.${system});
+        flake-utils.lib.eachDefaultSystem (system:
+          let pkgs = nixpkgs.legacyPackages.${system}; in {
 
+            packages = rec {
+              klassy = pkgs.stdenv.mkDerivation rec {
+                pname = "klassy";
+                version = "6.2.1";
 
-            klassy-qt6 = {pkgs, ...}: pkgs.stdenv.mkDerivation rec {
-              pname = "klassy";
-              version = "6.2.1";
-
-              src = pkgs.fetchFromGitHub {
-                owner = "paulmcauley";
-                repo = pname;
-                rev = "6.2.breeze6.2.1";
-                sha256 = "sha256-tFqze3xN1XECY74Gj0nScis7DVNOZO4wcfeA7mNZT5M=";
-              };
-
-              patches = [
-                ./project-version.patch
-              ];
-
-              cmakeFlags = ["-DBUILD_TESTING=OFF" "-DBUILD_QT5=OFF"];
-
-              nativeBuildInputs = with pkgs; [cmake kdePackages.extra-cmake-modules];
-
-              buildInputs = with pkgs.kdePackages; [
-                wrapQtAppsHook
-                kdecoration
-                kcoreaddons
-                kguiaddons
-                kconfigwidgets
-                kiconthemes
-                kwayland
-                kwindowsystem
-                kirigami
-                frameworkintegration
-                kcmutils
-                qtsvg
-                qtbase
-                qtdeclarative
-              ] ++ ( with pkgs; [
-                hicolor-icon-theme
-                xdg-utils
-              ]);
-
-              meta = with pkgs.lib; {
-                description = "A highly customizable binary Window Decoration and Application Style plugin for recent versions of the KDE Plasma desktop";
-                homepage = "https://github.com/paulmcauley/klassy";
-                license = with licenses; [gpl2Only gpl2Plus gpl3Only bsd3 mit];
-              };
-            };
-      in {
-
-        packages = eachSystem
-                (pkgs: {
-                default = klassy-qt6 {
-                    inherit pkgs;
+                src = pkgs.fetchFromGitHub {
+                  owner = "Foxinatel";
+                  repo = pname;
+                  rev = "plasma6.2";
+                  sha256 = "sha256-9IZhO8a8URTYPv6/bf7r3incfN1o2jBd2+mLVptNRYo=";
                 };
-                klassy-qt6 = klassy-qt6 { inherit pkgs; };
-                });
 
-      };
+                patches = [
+                  ./project-version.patch
+                ];
+
+                cmakeFlags = ["-DBUILD_TESTING=OFF" "-DBUILD_QT5=OFF"];
+
+                nativeBuildInputs = with pkgs; [cmake kdePackages.extra-cmake-modules];
+
+                buildInputs = with pkgs.kdePackages; [
+                  wrapQtAppsHook
+                  kdecoration
+                  kcoreaddons
+                  kguiaddons
+                  kconfigwidgets
+                  kiconthemes
+                  kwayland
+                  kwindowsystem
+                  kirigami
+                  frameworkintegration
+                  kcmutils
+                  qtsvg
+                  qtbase
+                  qtdeclarative
+                ] ++ ( with pkgs; [
+                  hicolor-icon-theme
+                  xdg-utils
+                ]);
+
+                meta = with pkgs.lib; {
+                  description = "A highly customizable binary Window Decoration and Application Style plugin for recent versions of the KDE Plasma desktop";
+                  homepage = "https://github.com/paulmcauley/klassy";
+                  license = with licenses; [gpl2Only gpl2Plus gpl3Only bsd3 mit];
+                };
+              };
+              default = klassy;
+            };
+        });
 }
